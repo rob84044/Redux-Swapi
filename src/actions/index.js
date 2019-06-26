6d4a1776cef5e89e5fc36cc3e90d874d6a617ff8
Robert Pedersen <rob84044@users.noreply.github.com>
2019-06-25 20:17:09 -0600

    Set up actions/index.js file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,20 @@
+import axios from 'axios';
 // we'll need axios
+
+export const FETCH_START = 'FETCH_START';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAIL = 'FETCH_FAIL';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+
+export const getPeople = () => dispatch => {
+  dispatch({ type: FETCH_START });
+  axios
+    .get('https://pokeapi.co/api/v2/pokemon/')
+    .then(res => dispatch({ type: FETCH_SUCCESS, payload: res.data.results }))
+    .catch(err => dispatch({ type: FETCH_FAIL, payload: err }));
+};
 
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
